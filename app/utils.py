8b7748311150d4c92594626a5b5bbeb35e03e5db
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-07-02 14:39:10 +0530

    add fake data

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,4 @@
+from typing import TypedDict
 from aiohttp import ClientSession
 from fastapi import Header, HTTPException
 
@@ -12,3 +13,44 @@ async def verify_auth(Authorization: str | None = Header(default=None)):
 async def verify_hip(X_HIP_ID: str | None = Header(default=None)):
     if not X_HIP_ID:
         raise HTTPException(status_code=400, detail="X-HIP-ID invalid")
+
+
+class CareContext(TypedDict):
+    reference_number: str
+    display: str
+    details: str
+
+
+class Patient(TypedDict):
+    mobile: str
+    name: str
+    gender: str
+    id: str
+    abha: str
+    linked: bool
+    care_contexts: list[CareContext]
+
+
+class Facility(TypedDict):
+    name: str
+    id: str
+    patients: list[Patient]
+
+
+db: list[Facility] = [
+    {
+        "id": "f1",
+        "name": "facility #1",
+        "patients": [
+            {
+                "id": "p1",
+                "name": "patient 1",
+                "mobile": "999999999",
+                "abha": None,
+                "gender": "M",
+                "linked": False,
+                "care_contexts": [],
+            },
+        ],
+    }
+]
