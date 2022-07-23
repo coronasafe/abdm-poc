7f6b4ce70501b210054fe60f8329117b6f6fcf6e
Marmik Patel <44599895+Marmik2003@users.noreply.github.com>
2022-07-23 08:58:13 +0000

    added link facility endpoint

--- app/routers/facilities.py
+++ app/routers/facilities.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Response, status
 from pydantic import HttpUrl, BaseModel
 from ..utils.apis import gateway
@@ -30,6 +31,30 @@ class FacilityData(BaseModel):
 async def set_facility(req: FacilityData):
     try:
         await gateway.call("/devservice/v1/bridges/services", data=req.json())
+    except Exception as e:
+        print(e)
+    return Response(status_code=status.HTTP_202_ACCEPTED)
+
+
+class FacilityEndpoint(BaseModel):
+    address: str
+    connectionType: str
+    use: str
+
+
+class FacilityLinkData(BaseModel):
+    id: str
+    name: str
+    type: str
+    active: bool
+    alias: List[str]
+    endpoints: List[FacilityEndpoint]
+
+
+@router.post('/link-facility')
+async def link_facility(req: FacilityLinkData):
+    try:
+        await gateway.call("/v1/bridges/addUpdateServices", data=req.json())
     except Exception as e:
         print(e)
     return Response(status_code=status.HTTP_202_ACCEPTED)
